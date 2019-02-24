62633d8d4c7bfabe0021ec4c2ec282f9733592e3
Jan Nash <jnash@jnash.de>
2019-02-24 14:12:37 +0100

    Create macOS commandline tool project with Xcode

---
+++ PracticeRoom/PracticeRoom/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PracticeRoom
+//
+//  Created by Jan Nash on 24.02.19.
+//  Copyright © 2019 Jan Nash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
