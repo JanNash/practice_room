77c76a2caa75e2f9a451b8b025ec67e303c173e1
Jan Nash <jnash@jnash.de>
2019-02-25 08:48:17 +0100

    Create mainTests file with stub test function

---
+++ PracticeRoomTests/mainTests.swift
@@ -0,0 +1,14 @@
+//
+//  mainTests.swift
+//  PracticeRoomTests
+//
+//  Created by Jan Nash on 25.02.19.
+//  Copyright © 2019 Jan Nash. All rights reserved.
+//
+
+import XCTest
+
+
+class MainTests: XCTestCase {
+    func test() {}
+}
